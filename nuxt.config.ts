13f5cde4f98fc8937510b7be78b56469baaceca9
Dexter Alan Balderama <balderamadexteralan@gmail.com>
2025-08-29 22:27:14 +0800

    setup fonts and nuxt image

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,12 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
-  modules: ['@nuxt/eslint', '@nuxt/icon'],
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxt/fonts',
+  ],
   eslint: {
     config: {
       standalone: false,
@@ -14,7 +19,43 @@ export default defineNuxtConfig({
   vite: {
     plugins: [
       tailwindcss(),
+      {
+        apply: 'build',
+        name: 'vite-plugin-ignore-sourcemap-warnings',
+        configResolved(config) {
+          const originalOnWarn = config.build.rollupOptions.onwarn;
+          config.build.rollupOptions.onwarn = (warning, warn) => {
+            if (
+              warning.code === 'SOURCEMAP_BROKEN'
+              && warning.plugin === '@tailwindcss/vite:generate:build'
+            ) {
+              return;
+            }
+
+            if (originalOnWarn) {
+              originalOnWarn(
+                warning,
+                warn,
+              );
+            }
+            else {
+              warn(
+                warning,
+              );
+            }
+          };
+        },
+      },
     ],
   },
-
+  fonts: {
+    families: [
+      {
+        name: 'Poppins',
+        provider: 'google',
+        weights: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+        display: 'swap',
+      },
+    ],
+  },
 });
