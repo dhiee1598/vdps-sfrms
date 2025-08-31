147cf40abc11876e7e5d8490839b4118acaee542
Dexter Alan Balderama <balderamadexteralan@gmail.com>
2025-08-31 14:32:29 +0800

    add seeder and fix redirect after success login

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,7 @@ export default withNuxt(antfu({
     semi: true,
     quotes: 'single',
   },
-  ignores: ["**/migrations/*"],
+  ignores: ['**/migrations/*', '**/seed/*'],
 }, {
   rules: {
     'vue/max-attributes-per-line': ['error', {
