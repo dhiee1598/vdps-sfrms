25c80f3250eec26c369f0544b3f2f2c246172b08
Amaro018 <gladi0018@gmail.com>
2025-09-04 13:15:16 +0800

    distinguish if the student is enrolled or not

--- server/api/private/student/index.get.ts
+++ server/api/private/student/index.get.ts
@@ -1,13 +1,30 @@
 import db from '~~/server/db';
+import { enrollments } from '~~/server/db/schema/enrollment-schema';
 import { students, studentSelectSchema } from '~~/server/db/schema/student-schema';
+import { eq, isNull } from 'drizzle-orm';
 
-export default defineEventHandler(async (_event) => {
-  const allStudents = await db.select().from(students);
+export default defineEventHandler(async (event) => {
+  const query = getQuery(event);
+
+  const enrolled = query.enrolled === 'true';
+  let allStudents;
+
+  if (enrolled) {
+    allStudents = await db
+      .select({
+        student: students,
+      })
+      .from(students)
+      .leftJoin(enrollments, eq(enrollments.student_id, students.id))
+      .where(isNull(enrollments.student_id)); // ✅ only students with no enrollment
+  }
+  else {
+    allStudents = await db.select().from(students);
+  }
 
   const parsedAllStudents = studentSelectSchema.array().parse(allStudents);
-
   return {
-    message: 'Students fetched successfull',
+    message: 'Students fetched successfully',
     data: parsedAllStudents,
   };
 });
