86b7531a532b9359faf1f567a673bae7e52cced9
Dexter Alan Balderama <balderamadexteralan@gmail.com>
2025-09-14 01:18:08 +0800

    done user update

--- server/db/schema/user-schema.ts
+++ server/db/schema/user-schema.ts
@@ -1,12 +1,14 @@
 import { int, mysqlTable, timestamp, varchar } from 'drizzle-orm/mysql-core';
-import { createInsertSchema, createSelectSchema } from 'drizzle-zod';
+import { createInsertSchema, createSelectSchema, createUpdateSchema } from 'drizzle-zod';
+import z from 'zod';
 
 const users = mysqlTable('users', {
   id: int('id').autoincrement().primaryKey(),
   name: varchar({ length: 255 }).notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
   password: varchar({ length: 255 }).notNull(),
-  role: varchar({ length: 255 }).notNull(),
+  profile_image: varchar({ length: 255 }),
+  role: varchar({ length: 255 }),
   createdAt: timestamp().defaultNow(),
 });
 
@@ -17,8 +19,17 @@ const userInsertSchema = createInsertSchema(users, {
 }).omit({
   id: true,
   createdAt: true,
+  profile_image: true,
+  role: true,
 });
 
 const userSelectSchema = createSelectSchema(users).omit({ password: true });
 
-export { userInsertSchema, users, userSelectSchema };
+const userUpdateSchema = createUpdateSchema(users)
+  .omit({ createdAt: true, id: true, role: true, password: true })
+  .extend({
+    old_password: z.string().optional(),
+    new_password: z.string().optional(),
+  });
+
+export { userInsertSchema, users, userSelectSchema, userUpdateSchema };
